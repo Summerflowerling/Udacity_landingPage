f4e0f3bc218b2a1652a441a0319b0eb2b04b5717
yuling <yulinglin@protonmail.com>
2021-01-04 11:37:36 +0800

    Generate at least 4 sections after the DOM is loaded. Generate the list items for navBar according to the section number that is being made. Add and remove ative style with querySelectorAll and made it more dynamically without using switch stament.

--- fend/projects/landing-page/js/app.js
+++ fend/projects/landing-page/js/app.js
@@ -31,7 +31,8 @@ const section1 = document.querySelector("#section1")
 const section2 = document.querySelector("#section2")
 const section3 = document.querySelector("#section3")
 const newContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin "
-let section 
+let totalSectionNumber = 3; //initial 
+let newList;
 
 
 
@@ -42,105 +43,73 @@ let section
  * 
 */
 
-/*Add the total list amount we need*/
-function addNavList(num){
-   
-    for (let i=1 ; i<=num ; i++){     
-        const newList = document.createElement("li")
-        newList.setAttribute("id","list-item"+i)
-        newList.innerText = "section"+i
-        navbarUl.appendChild(newList) 
-    }
-}
-addNavList(4)
+
+
 
 
 /*add new section and its children elements*/
-function createSection(orderOfSection){
-    const newSection = document.createElement("section")
-    newSection.innerHTML = 
-    `<section id="section${orderOfSection}"> 
+function createSection(numberOfSection){
+    
+    for(let i=1 ; i<=numberOfSection ; i++){
+        totalSectionNumber++;
+        let newSection = document.createElement("section")
+        newSection.innerHTML = 
+    `<section id="section${3+i}"> 
     <div class="landing__container"> 
-    <h2>Section ${orderOfSection}</h2>
-    <p class="section${orderOfSection}-p"></p>
+    <h2>Section ${3+i}</h2>
+    <p class="section${3+i}-p"></p>
     </div>
     </section>`
     sectionWrapper.appendChild(newSection)
-    section = "section"+orderOfSection
+  
+    }
 }
 
-/*get created list items in the Nav*/
-const listItem1 = document.querySelector("#list-item1")
-const listItem2 = document.querySelector("#list-item2")
-const listItem3 = document.querySelector("#list-item3")
-const listItem4 = document.querySelector("#list-item4")
+document.addEventListener("onload", createSection(3))
 
 
-
-document.addEventListener("click" ,function scroll(event){
+/*add listItem*/
+function addNavList(totalSectionNumber){
    
-    /*check if the list item has been created and if the corresponded section has been added to the DOM*/
-    if (event.target.id===`list-item${4}` && section==undefined){
-            createSection(4) 
-    }
-
-   
-    switch (event.target.id){
-        case "list-item1" :
-            section1.scrollIntoView(true);
-            listItem1.classList.add("underline")
-            listItem2.classList.remove("underline")
-            listItem3.classList.remove("underline")
-            listItem4.classList.remove("underline")
-            break;
-        case "list-item2":
-            section2.scrollIntoView(true);
-            section2.classList.add("your-active-class");
-            listItem2.classList.add("underline")
-            listItem1.classList.remove("underline")
-            listItem3.classList.remove("underline")
-            listItem4.classList.remove("underline")
-            
-            break;
-        case "list-item3":
-            section3.scrollIntoView(true);
-            section3.classList.add("your-active-class");
-            listItem3.classList.add("underline")
-            listItem1.classList.remove("underline")
-            listItem2.classList.remove("underline")
-            listItem4.classList.remove("underline")
-            
-            break;
-            
-        case "list-item4":
-            const section4 = document.querySelector("#section4")
-            
-            section4.scrollIntoView(true);
-            section4.classList.add("your-active-class");
-            document.querySelector(".section4-p").innerText = newContent;
-            listItem4.classList.add("underline")
-            listItem1.classList.remove("underline")
-            listItem2.classList.remove("underline")
-            listItem3.classList.remove("underline")
-            break;
-            default:
-                alert("Something is wrong with this section")
+    for (let i=1 ; i<=totalSectionNumber ; i++){     
+         newList = document.createElement("li")
+        newList.setAttribute("id","list-item"+i)
+        newList.textContent = `section${i}`
+        //newList.innerHTML = `<a href=# class="sectionLink">section ${i}</a>`
+        navbarUl.appendChild(newList) 
+        
         
     }
-})
+}
+
+       
+addNavList(totalSectionNumber);
+
+// Scroll to section on list item click
+navbarUl.addEventListener("click",function(event){
+    event.preventDefault()
+    
+    // remove active style to section when not being view
+    for (let i=0 ; i<document.querySelectorAll(".highLight").length ; i++){
+
+        document.querySelectorAll(".highLight")[i].classList.remove("highLight")
+
+     }
+     //add active style to the section that is being viewd
+    document.querySelector(`#${event.target.textContent}`).scrollIntoView()
+            event.target.classList.add("highLight")
+
+      
+}) 
 
 
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
 
 
 
-// Add class 'active' to section when near top of viewport
+
+
 
 
 // Scroll to anchor ID using scrollTO event
@@ -154,7 +123,7 @@ document.addEventListener("click" ,function scroll(event){
 
 // Build menu 
 
-// Scroll to section on link click
+
 
 // Set sections as active
 
